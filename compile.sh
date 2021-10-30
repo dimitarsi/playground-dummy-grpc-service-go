8f3ebef81c015744507f050e1501b6eab5ae3483
Dimitar Iliev <info@dimitarsi.me>
2021-10-30 13:15:06 +0000

    feat: include well know types

--- compile.sh
+++ compile.sh
@@ -1,2 +1,2 @@
 # protoc product_info.proto --go_opt=M./product_info.proto=github.com/dimitarsi/hello-grpc/service --go_out=. --go-grpc_out=.
-protoc protos/ecommerce/*.proto --go_out=. --go-grpc_out=.
+protoc --proto_path=. protos/ecommerce/*.proto --go_out=. --go-grpc_out=.
--- service/entities.pb.go
+++ service/entities.pb.go
@@ -130,6 +130,85 @@ func (x *ProductID) GetValue() string {
 	return ""
 }
 
+type Order struct {
+	state         protoimpl.MessageState
+	sizeCache     protoimpl.SizeCache
+	unknownFields protoimpl.UnknownFields
+
+	Id          string   `protobuf:"bytes,1,opt,name=id,proto3" json:"id,omitempty"`
+	Description string   `protobuf:"bytes,2,opt,name=description,proto3" json:"description,omitempty"`
+	Price       float32  `protobuf:"fixed32,3,opt,name=price,proto3" json:"price,omitempty"`
+	Items       []string `protobuf:"bytes,4,rep,name=items,proto3" json:"items,omitempty"`
+	Destination string   `protobuf:"bytes,5,opt,name=destination,proto3" json:"destination,omitempty"`
+}
+
+func (x *Order) Reset() {
+	*x = Order{}
+	if protoimpl.UnsafeEnabled {
+		mi := &file_protos_ecommerce_entities_proto_msgTypes[2]
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		ms.StoreMessageInfo(mi)
+	}
+}
+
+func (x *Order) String() string {
+	return protoimpl.X.MessageStringOf(x)
+}
+
+func (*Order) ProtoMessage() {}
+
+func (x *Order) ProtoReflect() protoreflect.Message {
+	mi := &file_protos_ecommerce_entities_proto_msgTypes[2]
+	if protoimpl.UnsafeEnabled && x != nil {
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		if ms.LoadMessageInfo() == nil {
+			ms.StoreMessageInfo(mi)
+		}
+		return ms
+	}
+	return mi.MessageOf(x)
+}
+
+// Deprecated: Use Order.ProtoReflect.Descriptor instead.
+func (*Order) Descriptor() ([]byte, []int) {
+	return file_protos_ecommerce_entities_proto_rawDescGZIP(), []int{2}
+}
+
+func (x *Order) GetId() string {
+	if x != nil {
+		return x.Id
+	}
+	return ""
+}
+
+func (x *Order) GetDescription() string {
+	if x != nil {
+		return x.Description
+	}
+	return ""
+}
+
+func (x *Order) GetPrice() float32 {
+	if x != nil {
+		return x.Price
+	}
+	return 0
+}
+
+func (x *Order) GetItems() []string {
+	if x != nil {
+		return x.Items
+	}
+	return nil
+}
+
+func (x *Order) GetDestination() string {
+	if x != nil {
+		return x.Destination
+	}
+	return ""
+}
+
 var File_protos_ecommerce_entities_proto protoreflect.FileDescriptor
 
 var file_protos_ecommerce_entities_proto_rawDesc = []byte{
@@ -143,8 +222,16 @@ var file_protos_ecommerce_entities_proto_rawDesc = []byte{
 	0x52, 0x0b, 0x64, 0x65, 0x73, 0x63, 0x72, 0x69, 0x70, 0x74, 0x69, 0x6f, 0x6e, 0x22, 0x21, 0x0a,
 	0x09, 0x50, 0x72, 0x6f, 0x64, 0x75, 0x63, 0x74, 0x49, 0x44, 0x12, 0x14, 0x0a, 0x05, 0x76, 0x61,
 	0x6c, 0x75, 0x65, 0x18, 0x01, 0x20, 0x01, 0x28, 0x09, 0x52, 0x05, 0x76, 0x61, 0x6c, 0x75, 0x65,
-	0x42, 0x0a, 0x5a, 0x08, 0x73, 0x65, 0x72, 0x76, 0x69, 0x63, 0x65, 0x2f, 0x62, 0x06, 0x70, 0x72,
-	0x6f, 0x74, 0x6f, 0x33,
+	0x22, 0x87, 0x01, 0x0a, 0x05, 0x4f, 0x72, 0x64, 0x65, 0x72, 0x12, 0x0e, 0x0a, 0x02, 0x69, 0x64,
+	0x18, 0x01, 0x20, 0x01, 0x28, 0x09, 0x52, 0x02, 0x69, 0x64, 0x12, 0x20, 0x0a, 0x0b, 0x64, 0x65,
+	0x73, 0x63, 0x72, 0x69, 0x70, 0x74, 0x69, 0x6f, 0x6e, 0x18, 0x02, 0x20, 0x01, 0x28, 0x09, 0x52,
+	0x0b, 0x64, 0x65, 0x73, 0x63, 0x72, 0x69, 0x70, 0x74, 0x69, 0x6f, 0x6e, 0x12, 0x14, 0x0a, 0x05,
+	0x70, 0x72, 0x69, 0x63, 0x65, 0x18, 0x03, 0x20, 0x01, 0x28, 0x02, 0x52, 0x05, 0x70, 0x72, 0x69,
+	0x63, 0x65, 0x12, 0x14, 0x0a, 0x05, 0x69, 0x74, 0x65, 0x6d, 0x73, 0x18, 0x04, 0x20, 0x03, 0x28,
+	0x09, 0x52, 0x05, 0x69, 0x74, 0x65, 0x6d, 0x73, 0x12, 0x20, 0x0a, 0x0b, 0x64, 0x65, 0x73, 0x74,
+	0x69, 0x6e, 0x61, 0x74, 0x69, 0x6f, 0x6e, 0x18, 0x05, 0x20, 0x01, 0x28, 0x09, 0x52, 0x0b, 0x64,
+	0x65, 0x73, 0x74, 0x69, 0x6e, 0x61, 0x74, 0x69, 0x6f, 0x6e, 0x42, 0x0a, 0x5a, 0x08, 0x73, 0x65,
+	0x72, 0x76, 0x69, 0x63, 0x65, 0x2f, 0x62, 0x06, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x33,
 }
 
 var (
@@ -159,10 +246,11 @@ func file_protos_ecommerce_entities_proto_rawDescGZIP() []byte {
 	return file_protos_ecommerce_entities_proto_rawDescData
 }
 
-var file_protos_ecommerce_entities_proto_msgTypes = make([]protoimpl.MessageInfo, 2)
+var file_protos_ecommerce_entities_proto_msgTypes = make([]protoimpl.MessageInfo, 3)
 var file_protos_ecommerce_entities_proto_goTypes = []interface{}{
 	(*Product)(nil),   // 0: ecommerce.Product
 	(*ProductID)(nil), // 1: ecommerce.ProductID
+	(*Order)(nil),     // 2: ecommerce.Order
 }
 var file_protos_ecommerce_entities_proto_depIdxs = []int32{
 	0, // [0:0] is the sub-list for method output_type
@@ -202,6 +290,18 @@ func file_protos_ecommerce_entities_proto_init() {
 				return nil
 			}
 		}
+		file_protos_ecommerce_entities_proto_msgTypes[2].Exporter = func(v interface{}, i int) interface{} {
+			switch v := v.(*Order); i {
+			case 0:
+				return &v.state
+			case 1:
+				return &v.sizeCache
+			case 2:
+				return &v.unknownFields
+			default:
+				return nil
+			}
+		}
 	}
 	type x struct{}
 	out := protoimpl.TypeBuilder{
@@ -209,7 +309,7 @@ func file_protos_ecommerce_entities_proto_init() {
 			GoPackagePath: reflect.TypeOf(x{}).PkgPath(),
 			RawDescriptor: file_protos_ecommerce_entities_proto_rawDesc,
 			NumEnums:      0,
-			NumMessages:   2,
+			NumMessages:   3,
 			NumExtensions: 0,
 			NumServices:   0,
 		},
