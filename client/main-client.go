b503205f2f1bb314ca73ed2f85ea9241bec5ea89
Dimitar Iliev <info@dimitarsi.me>
2021-10-31 09:38:59 +0100

    feat: implement streaming client to update orders

--- client/main-client.go
+++ client/main-client.go
@@ -40,4 +40,36 @@ func RunClient() {
 		log.Printf("Order: %v", order)
 	}
 
+	newOrders := []*service.Order{
+		 {
+			Description: "Order 1",
+			Items: []string{"Foobar"},
+		},
+		 {
+			Description: "Order 2",
+			Items: []string{"Foobar"},
+		},
+		 {
+			Description: "Order 3",
+			Items: []string{"Foobar"},
+		},
+	}
+	updateStream, err := orderManagementClient.UpdateOrders(ctx)
+
+	if err != nil {
+		log.Fatalf("Error sending data to UpdateOrders %v", err)
+	}
+	for _, order := range newOrders {
+		log.Printf("Sending new order...")
+		updateStream.Send(order)
+	}
+
+	response, err := updateStream.CloseAndRecv()
+
+	if err != nil {
+		log.Fatalf("Error receiving response from server %v", err)
+	}
+	
+	log.Printf("Server response\n\n -------> \n\n %v\n\n<-------\n", response.Value)
+
 }
